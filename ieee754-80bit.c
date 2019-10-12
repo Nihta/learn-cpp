4f4c4462c720ccda294435640532ad41b45d2c85
Nihta <Trvathin@gmail.com>
2019-10-12 18:13:39 +0700

    Update and fix some bug

--- ieee754-80bit.c
+++ ieee754-80bit.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <math.h>
+#include <windows.h>
+
+// Chỉnh thời gian delay giữa các bước
+#define delay Sleep(1000);
 
 // In size phần tử của mảng arr bắt đầu từ start
 void printArray(int arr[], int start, int size)
@@ -8,15 +12,25 @@ void printArray(int arr[], int start, int size)
         printf("%d", arr[i]);
 }
 
-// Tìm b
-int findB(int arr[], int size, int indexCurent)
+// Tìm b ( ... 2^b)
+int findB(int arr[], int size, int indexCommaCurent)
 {
     for (int i = 0; i < size; i++)
     {
         if (arr[i] == 1)
-            return abs(indexCurent - i - 1);
+            return indexCommaCurent - (i + 1);
     }
     return 0;
+}
+
+// Tìm vị trí đấu phẩy (ở bước 2)
+int findIndexComma(int binFloat[], int arrSize)
+{
+    int i;
+    for (i = 0; i < arrSize; i++)
+        if (binFloat[i] == 1)
+            break;
+    return i + 1;
 }
 
 // Tìm E và chuyển vào mảng kết quả
@@ -34,10 +48,10 @@ void handleE(int result[], int E)
 }
 
 // Tìm M và chuyển M vào mảng kết quả
-void handleM(int result[], int binFloat[], int indexDauPhay)
+void handleM(int result[], int binFloat[], int indexComma)
 {
     int index = 0;
-    for (int i = indexDauPhay + 1; i < 65 - indexDauPhay - 1; i++)
+    for (int i = indexComma + 1; i < 65 - indexComma - 1; i++)
         result[16 + index++] = binFloat[i];
 
     // Bổ sung số 0 cho đủ 64 bit (nếu thiếu)
@@ -45,44 +59,51 @@ void handleM(int result[], int binFloat[], int indexDauPhay)
         result[index++] = 0;
 }
 
-// Chuyển từ bin qua hex
-void toHex(char hex[], int result[])
+// In mã bin ra mà hình
+void printBin(int result[])
 {
-    int index = 0;
+    for (int i = 0; i < 80; i += 4)
+        printf("%d%d%d%d ", result[i], result[i + 1], result[i + 2], result[i + 3]);
+}
+
+// Chuyển từ bin qua hex và in ra màn hình
+void printBinToHex(int result[])
+{
     char dataBaseHex[15] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E'};
     for (int i = 0; i < 80; i += 4)
     {
         int value = result[i] * 8 + result[i + 1] * 4 + result[i + 2] * 2 + result[i + 3];
-        hex[index++] = dataBaseHex[value];
+        printf("%c", dataBaseHex[value]);
     }
+    printf("H");
 }
 
-void solution(double question)
+void solution(double X)
 {
-    if (question == 0)
+    if (X == 0)
     {
         printf("X = 00000000000000000000H");
         return;
     }
 
+    // Tách phần nguyên và phần thập phân
     long long n;
     double f;
-    if (question < 0)
+    if (X < 0)
     {
-        n = floor(-question);
-        f = -question - n;
+        n = floor(-X);
+        f = -X - n;
     }
     else
     {
-        n = floor(question);
-        f = question - n;
+        n = floor(X);
+        f = X - n;
     }
 
     int binFloat[65] = {0};
 
     // Chuyển phần nguyên sang nhị phân
     int tmp1[65], size1 = 0;
-
     while (n != 0)
     {
         tmp1[size1++] = n % 2;
@@ -93,16 +114,13 @@ void solution(double question)
         binFloat[i] = tmp1[size1 - 1 - i];
 
     // Chuyển phần thập phân sang nhị phân
-    int maxSize = 65 - size1;
-    int tmp2[maxSize] = {0}, size2 = 0;
-    while (f != 1.0 && size2 < maxSize && f != 0.0)
+    int tmp2[65] = {0}, size2 = 0;
+    while (size2 < 65 - size1 && f != 0.0)
     {
         f *= 2;
         if (f >= 1)
         {
             tmp2[size2++] = 1;
-            if (f == 1.0)
-                break;
             f -= 1;
         }
         else
@@ -114,33 +132,42 @@ void solution(double question)
     for (int i = 0; i < size2; i++)
         binFloat[size1 + i] = tmp2[i];
 
+    delay(1000);
+
     // Bước 1
-    printf("Buoc 1: X = ");
-    if (question < 0)
-        printf("-");
+    printf("Buoc 1: X = %c", X < 0 ? '-' : ' ');
+    // In dữ liệu
+    if (size1 == 0)
+        printf("0");
     printArray(binFloat, 0, size1);
-    printf(",");
+    if (size2 != 0)
+        printf(",");
     printArray(tmp2, 0, size2);
 
+    delay(1000);
+
     // Bước 2
-    printf("\nBuoc 2: X = ");
-    if (question < 0)
-        printf("-");
+    printf("\nBuoc 2: X = %c", X < 0 ? '-' : ' ');
+    // Tìm b
     int b = findB(binFloat, 65, size1);
-    int indexDauPhay = -1;
-    for (int i = 0; i < size1 + size2; i++)
-    {
+    // Tìm vị trí dấu phẩy
+    int indexComma = findIndexComma(binFloat, size1 + size2);
+    // In dữ liệu
+    printf("1,");
+    for (int i = indexComma; i < size1 + size2; i++)
         printf("%d", binFloat[i]);
-        if (binFloat[i] == 1 && indexDauPhay == -1)
-            printf(","), indexDauPhay = i;
-    }
-    printf(" * 2^%d\n", b);
+    if (indexComma == size1 + size2)
+        printf("0");
+    printf(" * 2^(%d)", b);
+
+    delay(1000);
 
     // Bước 3
-    // Tìm S
     int result[80] = {0};
-    printf("Buoc 3: Ta co:\n");
-    if (question >= 0)
+    printf("\nBuoc 3: Ta co:\n");
+    delay(1000);
+    // Tìm S
+    if (X >= 0)
     {
         result[0] = 0;
         printf("\tS = 0, vi X > 0\n");
@@ -150,36 +177,43 @@ void solution(double question)
         result[0] = 1;
         printf("\tS = 1, vi X < 0\n");
     }
+    delay(1000);
     // Tìm E
     int E = b + 16383;
     printf("\tE - 16383 = %d => E = %d = ", b, E);
     handleE(result, E);
     printArray(result, 1, 15);
+    delay(1000);
     // Tìm M
     printf("\n\tM = ");
-    handleM(result, binFloat, indexDauPhay);
+    handleM(result, binFloat, indexComma);
     printArray(result, 16, 64);
 
-    // Bước 4
-    printf("\n => X = ");
-    for (int i = 0; i < 80; i += 4)
-        printf("%d%d%d%d ", result[i], result[i + 1], result[i + 2], result[i + 3]);
+    delay(1000);
 
+    // Bước 4
+    // In mã bin
+    printf("\n => X = ");
+    printBin(result);
+    delay(1000);
+    // In mã hex
     printf("\n      = ");
-    char hex[20];
-    toHex(hex, result);
-    for (int i = 0; i < 20; i++)
-        printf("%c", hex[i]);
-    printf("H");
+    printBinToHex(result);
 }
 
 int main()
 {
-    double question;
+    double X;
     printf("Nhap X = ");
-    scanf("%lf", &question);
+    scanf("%lf", &X);
 
-    solution(question);
+    system("cls");
+
+    printf("%lf -> IEEE 754/85 (80bit)\n", X);
+    solution(X);
+
+    printf("\n===============================================================");
+    printf("============================================");
 
     return 0;
 }
